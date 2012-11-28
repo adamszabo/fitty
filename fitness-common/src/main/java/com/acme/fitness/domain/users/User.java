77f1fbf0a74254788851e72c1ab4276696b05cdb
adamszabo <devdi@freemail.hu>
2012-11-28 15:27:08 +0100

    create tests for User in common module

--- fitness-common/src/main/java/com/acme/fitness/domain/users/User.java
+++ fitness-common/src/main/java/com/acme/fitness/domain/users/User.java
@@ -77,6 +77,88 @@ public class User {
 				+ lastLoginIp + ", isEnabled=" + enabled + ", numberOfRetries="
 				+ numberOfRetries + "]";
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (enabled ? 1231 : 1237);
+		result = prime * result
+				+ ((fullName == null) ? 0 : fullName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result
+				+ ((lastLogin == null) ? 0 : lastLogin.hashCode());
+		result = prime * result
+				+ ((lastLoginIp == null) ? 0 : lastLoginIp.hashCode());
+		result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+		result = prime * result + numberOfRetries;
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		result = prime * result
+				+ ((registration == null) ? 0 : registration.hashCode());
+		result = prime * result
+				+ ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof User))
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (enabled != other.enabled)
+			return false;
+		if (fullName == null) {
+			if (other.fullName != null)
+				return false;
+		} else if (!fullName.equals(other.fullName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastLogin == null) {
+			if (other.lastLogin != null)
+				return false;
+		} else if (!lastLogin.equals(other.lastLogin))
+			return false;
+		if (lastLoginIp == null) {
+			if (other.lastLoginIp != null)
+				return false;
+		} else if (!lastLoginIp.equals(other.lastLoginIp))
+			return false;
+		if (mobile == null) {
+			if (other.mobile != null)
+				return false;
+		} else if (!mobile.equals(other.mobile))
+			return false;
+		if (numberOfRetries != other.numberOfRetries)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (registration == null) {
+			if (other.registration != null)
+				return false;
+		} else if (!registration.equals(other.registration))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
 
 	public long getId() {
 		return id;
--- fitness-common/src/test/java/com/acme/fitness/domain/UserTest.java
+++ fitness-common/src/test/java/com/acme/fitness/domain/UserTest.java
@@ -1,9 +1,88 @@
 package com.acme.fitness.domain;
 
+import java.util.Date;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.acme.fitness.domain.users.User;
+
 public class UserTest {
+	private User underTest;
+
+	@Before
+	public void setUp() {
+		underTest = new User();
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfId() {
+		underTest.setId(1L);
+		Assert.assertEquals(1L, underTest.getId());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfUserName() {
+		underTest.setUserName("USERNAME");
+		Assert.assertEquals("USERNAME", underTest.getUserName());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfPassword() {
+		underTest.setPassword("PASSWORD");
+		Assert.assertEquals("PASSWORD", underTest.getPassword());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfEmail() {
+		underTest.setEmail("EMAIL@EMAIL.COM");
+		Assert.assertEquals("EMAIL@EMAIL.COM", underTest.getEmail());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfMobile() {
+		underTest.setMobile("MOBILENUMBER");
+		Assert.assertEquals("MOBILENUMBER", underTest.getMobile());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfRegistration() {
+		Date expected=new Date();
+		underTest.setRegistration(expected);
+		Assert.assertEquals(expected, underTest.getRegistration());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfLastLogin() {
+		Date expected=new Date();
+		underTest.setLastLogin(expected);
+		Assert.assertEquals(expected, underTest.getLastLogin());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfLastLoginIp() {
+		underTest.setLastLoginIp("LASTIP");
+		Assert.assertEquals("LASTIP", underTest.getLastLoginIp());		
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfEnabled() {
+		underTest.setEnabled(true);
+		Assert.assertEquals(true, underTest.isEnabled());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfNumberOfEntries() {
+		underTest.setNumberOfRetries(1);
+		Assert.assertEquals(1, underTest.getNumberOfRetries());
+	}
 	
-	
-	public void setUp(){
-		
+	@Test
+	public void testEqualsWithEqualsVeryfier(){
+		EqualsVerifier.forClass(User.class).suppress(Warning.STRICT_INHERITANCE).verify();
 	}
 }
