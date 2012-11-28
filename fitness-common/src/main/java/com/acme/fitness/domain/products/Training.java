61e0433b7129d55da1e0b00d0b54612f4a914509
adamszabo <devdi@freemail.hu>
2012-11-28 17:50:33 +0100

    create test for Training class

--- fitness-common/src/main/java/com/acme/fitness/domain/products/Training.java
+++ fitness-common/src/main/java/com/acme/fitness/domain/products/Training.java
@@ -69,6 +69,69 @@ public class Training {
 				+ ", isAnalyzed=" + isAnalyzed + ", basketId=" + basket.getId()
 				+ "]";
 	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((basket == null) ? 0 : basket.hashCode());
+		result = prime * result + burnedCalories;
+		result = prime * result + ((client == null) ? 0 : client.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + (isAnalyzed ? 1231 : 1237);
+		result = prime * result + ((review == null) ? 0 : review.hashCode());
+		result = prime * result + ((trainer == null) ? 0 : trainer.hashCode());
+		result = prime
+				* result
+				+ ((trainingStartDate == null) ? 0 : trainingStartDate
+						.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Training))
+			return false;
+		Training other = (Training) obj;
+		if (basket == null) {
+			if (other.basket != null)
+				return false;
+		} else if (!basket.equals(other.basket))
+			return false;
+		if (burnedCalories != other.burnedCalories)
+			return false;
+		if (client == null) {
+			if (other.client != null)
+				return false;
+		} else if (!client.equals(other.client))
+			return false;
+		if (id != other.id)
+			return false;
+		if (isAnalyzed != other.isAnalyzed)
+			return false;
+		if (review == null) {
+			if (other.review != null)
+				return false;
+		} else if (!review.equals(other.review))
+			return false;
+		if (trainer == null) {
+			if (other.trainer != null)
+				return false;
+		} else if (!trainer.equals(other.trainer))
+			return false;
+		if (trainingStartDate == null) {
+			if (other.trainingStartDate != null)
+				return false;
+		} else if (!trainingStartDate.equals(other.trainingStartDate))
+			return false;
+		return true;
+	}
 
 	public long getId() {
 		return id;
---
+++ fitness-common/src/test/java/com/acme/fitness/domain/products/TrainingTest.java
@@ -0,0 +1,85 @@
+package com.acme.fitness.domain.products;
+
+import java.util.Date;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.acme.fitness.domain.orders.Basket;
+import com.acme.fitness.domain.users.User;
+
+public class TrainingTest {
+	private Training underTest;
+	
+	@Mock
+	private User user;
+	
+	@Mock
+	private Basket basket;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		underTest = new Training();
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfId() {
+		underTest.setId(1L);
+		Assert.assertEquals(1L, underTest.getId());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfTrainer() {
+		underTest.setTrainer(user);
+		Assert.assertEquals(user, underTest.getTrainer());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfClient() {
+		underTest.setClient(user);
+		Assert.assertEquals(user, underTest.getClient());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfTrainingStartDate() {
+		Date expected=new Date();
+		underTest.setTrainingStartDate(expected);
+		Assert.assertEquals(expected, underTest.getTrainingStartDate());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfIsAnalyzed() {
+		underTest.setAnalyzed(true);
+		Assert.assertEquals(true, underTest.isAnalyzed());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfBurnedCalories() {
+		underTest.setBurnedCalories(100);
+		Assert.assertEquals(100, underTest.getBurnedCalories());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfReview() {
+		underTest.setReview("TESTREVIEW");
+		Assert.assertEquals("TESTREVIEW", underTest.getReview());
+	}
+
+	@Test
+	public void testGetterSetterBehaviourOfBasket() {
+		underTest.setBasket(basket);
+		Assert.assertEquals(basket, underTest.getBasket());
+	}
+	
+	@Test
+	public void testEqualsWithEqualsVeryfier(){
+		EqualsVerifier.forClass(Training.class).suppress(Warning.STRICT_INHERITANCE).suppress(Warning.NULL_FIELDS).verify();
+	}
+}
