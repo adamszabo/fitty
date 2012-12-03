5274f78b25801e61f9fa953c43cc5a6cc37f2a62
adamszabo <devdi@freemail.hu>
2012-12-03 15:54:16 +0100

    add debug level logging to the save, update and delete methods of the DAO classes

--- fitness-dao/src/main/java/com/acme/fitness/dao/hibernate/AbstractHibernateGenericDao.java
+++ fitness-dao/src/main/java/com/acme/fitness/dao/hibernate/AbstractHibernateGenericDao.java
@@ -2,6 +2,8 @@ package com.acme.fitness.dao.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.acme.fitness.dao.GenericDao;
@@ -11,19 +13,24 @@ public abstract class AbstractHibernateGenericDao<T> implements GenericDao<T>{
 	@Autowired
 	private SessionFactory sessionFactory;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	protected Session getSession(){
 		return sessionFactory.getCurrentSession();
 	}
 	
 	public void save(T t){
 		getSession().persist(t);
+		logger.debug("Entity SAVED in the database: " + t);
 	}
 	
 	public void update(T t){
 		getSession().update(t);
+		logger.debug("Entity UPDATED in the database: " + t);
 	}
 	
 	public void delete(T t){
 		getSession().delete(t);
+		logger.debug("Entity DELETED in the database: " + t);
 	}
 }
