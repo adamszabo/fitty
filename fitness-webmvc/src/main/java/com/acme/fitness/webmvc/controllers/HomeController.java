91e7cec00efe66c4183e0a758bdab977caac53fc
adamszabo <devdi@freemail.hu>
2012-12-11 10:30:34 +0100

    creating template for bootstrap

--- fitness-webmvc/src/main/java/com/acme/fitness/webmvc/controllers/HomeController.java
+++ fitness-webmvc/src/main/java/com/acme/fitness/webmvc/controllers/HomeController.java
@@ -32,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("kiskutya", "dándog");
 		
 		return "index";
 	}
