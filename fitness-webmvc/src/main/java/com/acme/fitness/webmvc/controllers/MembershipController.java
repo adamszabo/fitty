3b83ab4b1953171e65b57148df8060e1ee910d84
adamszabo <devdi@freemail.hu>
2013-01-24 11:45:25 +0100

    add delete memebership from basket

--- fitness-webmvc/src/main/java/com/acme/fitness/webmvc/controllers/MembershipController.java
+++ fitness-webmvc/src/main/java/com/acme/fitness/webmvc/controllers/MembershipController.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -38,8 +40,7 @@ import com.acme.fitness.webmvc.web.JsonManager;
 @RequestMapping("/berletek")
 public class MembershipController {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(WebShopController.class);
+	private static final Logger logger = LoggerFactory.getLogger(WebShopController.class);
 
 	@Autowired
 	private GeneralProductsService gps;
@@ -51,18 +52,15 @@ public class MembershipController {
 	private GeneralUsersService gus;
 
 	@RequestMapping("")
-	public String defaultPage(HttpServletResponse response,
-			HttpServletRequest request, Model model) {
-		addBasketToSessionIfExists(request, new ObjectMapper());
+	public String defaultPage(HttpServletResponse response, HttpServletRequest request, Model model) {
+		addBasketToSessionIfExists(request, response, new ObjectMapper());
 		model.addAttribute("membershipTypes", gps.getAllMembershipTypes());
 		return "berletek";
 	}
 
 	@RequestMapping("/ujberlet")
-	public String newMembership(
-			@RequestParam("membershipId") long membershipId,
-			HttpServletResponse response, HttpServletRequest request) {
-		
+	public String newMembership(@RequestParam("membershipId") long membershipId, HttpServletResponse response, HttpServletRequest request) {
+
 		ObjectMapper mapper = new ObjectMapper();
 
 		Map<String, String> map = readMembershipsBasketToMapFromCookies(request, mapper);
@@ -77,74 +75,101 @@ public class MembershipController {
 
 		return "redirect:/berletek";
 	}
-	
+
 	@RequestMapping("/megrendel")
 	public String checkOut(HttpServletResponse response, HttpServletRequest request) throws FitnessDaoException {
-		
-		Basket basket = (Basket)request.getSession().getAttribute("basket");
+
+		Basket basket = (Basket) request.getSession().getAttribute("basket");
 		if (!getUserName().equals("anonymousUser")) {
 			basket.setUser(gus.getUserByUsername(getUserName()));
 			try {
 				gos.checkOutBasket(basket);
+				deleteMembershipFromCookie(request, response);
+				request.getSession().removeAttribute("basket");
 			} catch (StoreQuantityException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return "redirect:/berletek";
 	}
 	
+	@RequestMapping("/torol")
+	public String delete(HttpServletResponse response, HttpServletRequest request) {
+		System.out.println("Töröljééé");
+		deleteMembershipFromCookie(request, response);
+		return "redirect:/berletek";
+		
+	}
+
 	private String getUserName() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		return auth.getName();
 	}
-	
-	private void addBasketToSessionIfExists(HttpServletRequest request,
-			ObjectMapper mapper) {
+
+	private void addBasketToSessionIfExists(HttpServletRequest request, HttpServletResponse response, ObjectMapper mapper) {
 		Basket basket = gos.newBasket(new User());
-		Map<String, Integer> products = readProductsBasketToMapFromCookies(
-				request, mapper);
+		boolean isBasketExists = false;
+		Map<String, Integer> products = readProductsBasketToMapFromCookies(request, mapper);
 		if (products.size() > 0) {
 			loadBasketWithProducts(products, basket);
+			isBasketExists = true;
 		}
-		Map<String, String> memberships = readMembershipsBasketToMapFromCookies(request,
-				mapper);
+		Map<String, String> memberships = readMembershipsBasketToMapFromCookies(request, mapper);
 		if (memberships.size() > 0) {
-			loadBasketWithMembership(memberships, basket);
-			System.out.println(basket.getMemberships());
+			try {
+				loadBasketWithMembership(memberships, basket);
+				isBasketExists = true;
+			} catch (FitnessDaoException e) {
+				deleteMembershipFromCookie(request, response);
+			}
 		}
-		request.getSession().setAttribute("basket", basket);
-		
+		if (isBasketExists) {
+			request.getSession().setAttribute("basket", basket);
+		} else {
+			deleteMembershipFromCookie(request, response);
+			request.getSession().removeAttribute("basket");
+		}
 	}
 
-	private void loadBasketWithMembership(Map<String, String> memberships,
-			Basket basket) {
+	private void deleteMembershipFromCookie(HttpServletRequest request, HttpServletResponse response) {
+		request.getSession().removeAttribute("memberhipsInBasket");
+		for(Cookie c :request.getCookies()) {
+			if(c.getName().equals("memberhipsInBasket")) {
+				c.setMaxAge(0);
+				response.addCookie(c);
+			}
+		}
+	}
+
+	private void loadBasketWithMembership(Map<String, String> memberships, Basket basket) throws FitnessDaoException {
 		Membership membership = parseMembership(memberships);
-		basket.addMembership(membership);
-		membership.setBasket(basket);
+		if (membership != null) {
+			basket.addMembership(membership);
+			membership.setBasket(basket);
+		} else {
+			memberships = new HashMap<String, String>();
+
+		}
 	}
 
-	private Membership parseMembership(Map<String, String> memberships) {
+	private Membership parseMembership(Map<String, String> memberships) throws FitnessDaoException {
 		Long membershipId = null;
 		Membership membership = null;
 		MembershipType membershipType = null;
-		
+
 		for (String s : memberships.keySet()) {
 			membershipId = Long.parseLong(s);
 		}
-		try {
-			membershipType = gps.getMembershipTypeById(membershipId);
-		} catch (FitnessDaoException e) {
-			e.printStackTrace();
-		}
-		if(membershipType.getIsIntervally()) {
+		membershipType = gps.getMembershipTypeById(membershipId);
+		if (membershipType.getIsIntervally()) {
 			Date startDate = parseStringIntoDate(memberships.get(membershipId.toString()));
-			membership = gps.newMemberShip(membershipType.getIsIntervally(), membershipType.getDetail(), membershipType.getMaxNumberOfEntries(), startDate, new Date(startDate.getTime() + (long)membershipType.getExpireDateInDays()*1000*60*60*24), membershipType.getPrice());
+			membership = gps.newMemberShip(membershipType.getIsIntervally(), membershipType.getDetail(), membershipType.getMaxNumberOfEntries(), startDate,
+					new Date(startDate.getTime() + (long) membershipType.getExpireDateInDays() * 1000 * 60 * 60 * 24), membershipType.getPrice());
 		}
 		membership = gps.newMemberShip(membershipType.getIsIntervally(), membershipType.getDetail(), membershipType.getMaxNumberOfEntries(), null, null, membershipType.getPrice());
 		return membership;
 	}
-
 
 	private void loadBasketWithProducts(Map<String, Integer> map, Basket basket) {
 		for (String s : map.keySet()) {
@@ -152,8 +177,7 @@ public class MembershipController {
 		}
 	}
 
-	private void addProductToBasketByProductId(Basket basket, long id,
-			Integer quantity) {
+	private void addProductToBasketByProductId(Basket basket, long id, Integer quantity) {
 		Product product = null;
 		try {
 			product = gps.getProductById(id);
@@ -167,8 +191,7 @@ public class MembershipController {
 
 	}
 
-	private void writeMapToCookies(HttpServletResponse response,
-			ObjectMapper mapper, Map<String, String> map) {
+	private void writeMapToCookies(HttpServletResponse response, ObjectMapper mapper, Map<String, String> map) {
 
 		JsonManager<String> jsonManager = new JsonManager<String>(mapper);
 		String json = jsonManager.wrapToJsonString(map);
@@ -203,31 +226,26 @@ public class MembershipController {
 		return isInterval;
 	}
 
-	private Map<String, String> readMembershipsBasketToMapFromCookies(
-			HttpServletRequest request, ObjectMapper mapper) {
+	private Map<String, String> readMembershipsBasketToMapFromCookies(HttpServletRequest request, ObjectMapper mapper) {
 		String cookieValue = null;
 
 		CookieManager cookieManager = new CookieManager();
-		cookieValue = cookieManager.readFromCookies(request,
-				"memberhipsInBasket");
-
+		cookieValue = cookieManager.readFromCookies(request, "memberhipsInBasket");
+		
 		JsonManager<String> jsonManager = new JsonManager<String>(mapper);
 		Map<String, String> map = jsonManager.unwrapFromJsonString(cookieValue);
 
 		return map;
 	}
 
-	private Map<String, Integer> readProductsBasketToMapFromCookies(
-			HttpServletRequest request, ObjectMapper mapper) {
+	private Map<String, Integer> readProductsBasketToMapFromCookies(HttpServletRequest request, ObjectMapper mapper) {
 		String cookieValue = null;
 
 		CookieManager cookieManager = new CookieManager();
-		cookieValue = cookieManager
-				.readFromCookies(request, "productsInBasket");
+		cookieValue = cookieManager.readFromCookies(request, "productsInBasket");
 
 		JsonManager<Integer> jsonManager = new JsonManager<Integer>(mapper);
-		Map<String, Integer> map = jsonManager
-				.unwrapFromJsonString(cookieValue);
+		Map<String, Integer> map = jsonManager.unwrapFromJsonString(cookieValue);
 
 		return map;
 	}
--- fitness-webmvc/src/main/webapp/WEB-INF/ftl/template/template-loadScripts.ftl
+++ fitness-webmvc/src/main/webapp/WEB-INF/ftl/template/template-loadScripts.ftl
@@ -3,7 +3,8 @@
 	<!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
     <script src="<@spring.url relativeUrl="/resources/js/bootstrap-transition.js"/>"></script>
     <script src="<@spring.url relativeUrl="/resources/js/bootstrap-alert.js"/>"></script>
     <script src="<@spring.url relativeUrl="/resources/js/bootstrap-modal.js"/>"></script>
@@ -21,7 +22,6 @@
 	<script src="<@spring.url relativeUrl="/resources/js/slimScroll.js"/>"></script>
 	<script src="<@spring.url relativeUrl="/resources/js/bootstrap-datepicker.js"/>"></script>
 	
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
 	<script src="<@spring.url relativeUrl="/resources/js/jquery.selectBoxIt.js"/>"></script>
 	<script src="<@spring.url relativeUrl="/resources/js/bootstrap-fileupload.js"/>"></script>
 </#macro>
